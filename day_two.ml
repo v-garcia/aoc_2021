2d0ecb73656e4d342bf95f63874f5a84ee46caab
Vincent Garcia <VGarcia@sierrawireless.com>
2021-12-02 09:40:56 +0100

    Add res 2

--- day_two.ml
+++ day_two.ml
@@ -7,3 +7,13 @@ let rec navigate = function
                           | Up y' -> navigate (ns, (x, y - y'))
                           | Down y' -> navigate (ns, (x, y + y'))
 let res_1 = let (_, (x, y)) = navigate (Day_two_input.command_list, (0, 0)) in x * y
+
+let rec navigate' = function
+       | [], t -> [], t
+       | n::ns, (x, y, z) -> match n with 
+                                   | Down v -> navigate' (ns, (x, y, (z + v)))
+                                   | Up v -> navigate' (ns, (x, y, (z - v)))
+                                   | Forward v -> navigate' (ns, ((x + v), (y + (z * v)), z))
+
+                                   
+let res_2 = let (_, (x, y,_)) = navigate' (Day_two_input.command_list, (0, 0, 0)) in x * y
