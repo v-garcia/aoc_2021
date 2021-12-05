ede132f577120acb59ac20b4f138b099ca828e61
Vincent Garcia <VGarcia@sierrawireless.com>
2021-12-05 14:53:56 +0100

    Add day five q2

--- day_five.clj
+++ day_five.clj
@@ -4,16 +4,38 @@
 
 (def input (->> (slurp "day_five_input.clj") str/split-lines (map (fn [l] (->> (re-seq #"\d+" l) (map #(Integer. %)) (split-at 2))))))
 
+(defn horizontal? [[[x] [x']]] (= x x'))
+(defn vertical? [[[_ y] [_ y']]] (= y y'))
+(defn diagonal? [[[x y] [x' y']]] (= (Math/abs (- x x')) (Math/abs (- y y'))))
+
+(defn range'
+  [l]
+  (let [tr    (if (pos? (apply - l)) reverse identity)
+        [x y] (tr l)]  (tr (range x (inc y)))))
+
 (defn expand-line
-  [[[x y] [x' y']]]
-  ((comp distinct concat)
-   (mapv vector (repeat x) (range (min y y') (inc (max y y'))))
-   (mapv vector (range (min x x') (inc (max x x'))) (repeat y))))
+  [[[x y] [x' y'] :as l]]
+  (cond
+    (horizontal? l) (mapv vector (repeat x) (range' [y y']))
+    (vertical? l)   (mapv vector (range' [x x']) (repeat y))
+    (diagonal? l)   (mapv vector (range' [x x']) (range' [y y']))))
 
 (def q1
   (->>
    input
-   (filter (fn [[[x y] [x' y']]] (or (= x x') (= y y'))))
+   (filter (some-fn horizontal? vertical?))
+   (mapcat expand-line)
+   (reduce (fn [acc v] (update acc v (fnil inc 0))) {})
+   vals
+   (filter (partial < 1))
+   count))
+
+;; Question 2
+
+(def q2
+  (->>
+   input
+   (filter (some-fn horizontal? vertical? diagonal?))
    (mapcat expand-line)
    (reduce (fn [acc v] (update acc v (fnil inc 0))) {})
    vals
