11708dfdb54e66deab1bd3ed2786d05527c0330d
Vincent Garcia <VGarcia@sierrawireless.com>
2021-12-07 11:23:06 +0100

    Add day seven Q2

--- day_seven.clj
+++ day_seven.clj
@@ -1,11 +1,8 @@
 
 (require
- '[clojure.string :as str]
- '[clojure.set :as set])
+ '[clojure.string :as str])
 
 (def input (as-> (slurp "day_seven_input.txt") % (str/trim %) (str/split % #",") (mapv #(Integer. %) %)))
-
-()
 
 (defn get-pos->fuel
   [compare-fn]
@@ -16,8 +13,10 @@
     {} (range 0 1001))))
 
 ;; Q1
-
 (def pos->fuel (get-pos->fuel #(Math/abs (- %1 %2))))
 (def min-fuel (apply min (vals pos->fuel)))
 
 ;; Q2
+(def distance->fuel (reduce (fn [acc step] (conj acc (+ (peek acc) step))) [0] (range 1 2000)))
+(def pos->fuel' (get-pos->fuel #(distance->fuel (Math/abs (- %1 %2)))))
+(def min-fuel' (apply min (vals pos->fuel')))
